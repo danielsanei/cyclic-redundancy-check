915b8b98c06c77129c1fc5ef3c58d864d875a703
Sanei <Daniel.Sanei@commscope.com>
2025-07-18 11:10:51 -0700

    parse hex file complete

--- generate_hex_dump.cpp
+++ generate_hex_dump.cpp
@@ -1,7 +1,10 @@
 // imports
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <ctime>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 // generate binary file
@@ -11,8 +14,8 @@ void gen_bin_file() {
     ofstream binFile;
     
     // write to binary file, generate 10,000 random characters (file, mode flags)
-    binFile.open("bin_file.dat", ios::out | ios::binary );  // OR sets both bit flags to 1
-    if ( binFile ) {
+    binFile.open("bin_file.bin", ios::out | ios::binary );  // OR sets both bit flags to 1
+    if ( binFile ) {    // .dat is general data file (can be text or binary)
         
         // Generate random number
         srand(time(0)); // set seed to current time (seconds)
@@ -30,6 +33,71 @@ void gen_bin_file() {
     }
 }
 
+void parse_hex_file() {
+
+    // initialize input stream
+    ifstream binFile;
+    vector<string> hex_values;
+    
+    // retrieve file contents
+    binFile.open("sample_hex.hex");
+    if ( binFile ) {
+        
+        // store each line of hex file
+        while ( !binFile.eof() ) {  // continue until end of file
+            string line;
+            getline(binFile, line);
+            hex_values.push_back(line);
+        }
+        binFile.close();
+
+        // check hex values
+        for ( int i = 0; i < hex_values.size(); i++ ) {
+            cout << hex_values[i] << endl;
+        }
+
+    } else {
+        cout << "Error opening binary file.\n" << endl;
+    }
+
+    // extract data fields
+    //unordered_map<string, vector<string>> dataFields;   // declare map of data (hex, data fields)
+    vector<pair<string, vector<string>>> dataFields;
+    for ( int i = 0; i < hex_values.size(); i++ ) {
+
+        // get indices for variable-length data
+        int start = 9;
+        int end = (hex_values[i].size() - 2) - start;
+
+        // initialize extracted fields (ignore first semicolon)W
+        vector<string> currFields;
+        string byte_count = hex_values[i].substr(1, 2);
+        currFields.push_back(byte_count);
+        string address = hex_values[i].substr(4, 4);
+        currFields.push_back(address);
+        string record_type = hex_values[i].substr(7, 2);
+        currFields.push_back(record_type);
+        string data = hex_values[i].substr(start, end);
+        currFields.push_back(data);
+        string checksum = hex_values[i].substr(hex_values[i].size() - 2, 2);
+        currFields.push_back(checksum);
+        
+        // initialize map of data (hex, data fields)
+        dataFields.push_back(make_pair(hex_values[i], currFields));
+    }
+
+    // check extracted data
+    cout << endl << "Extracted Data" << endl << endl;
+    vector<string> fields = {"Byte Count: ", "Address: ", "Record Type: ", "Data: ", "Checksum: "};
+    for ( auto hex_value : dataFields ) {
+        cout << hex_value.first << endl;
+        for ( int i = 0; i < hex_value.second.size(); i++ ) {
+            cout << fields[i] << hex_value.second[i] << endl;
+        }
+        cout << endl;
+    }
+}
+
 // driver code
 int main() {
 
@@ -37,7 +105,10 @@ int main() {
     cout << char(99) << endl;
     
     // generate binary file with random characters (10 KB)
-    gen_bin_file();
+    //gen_bin_file();
+
+    // parse hex file
+    parse_hex_file();
 
     // return
     return 0;
