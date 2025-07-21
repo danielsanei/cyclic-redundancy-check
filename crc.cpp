01c7deb8c540d06540a4db077b0f37a972684fa9
Sanei <Daniel.Sanei@commscope.com>
2025-07-21 11:47:53 -0700

    hex to binary conversion

--- crc.cpp
+++ crc.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <vector>
 #include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 
 // generate binary file
@@ -34,7 +35,7 @@ void gen_bin_file() {
 }
 
 // extract data fields from hex file
-vector<pair<string, vector<string>>> parse_hex_file() {
+vector<string> parse_hex_file() {
 
     // initialize input stream
     ifstream binFile;
@@ -53,6 +54,8 @@ vector<pair<string, vector<string>>> parse_hex_file() {
         binFile.close();
 
         // check hex values
+        cout << endl << "Original Intel HEX file" << endl;
+        cout << "------------------------" << endl;
         for ( int i = 0; i < hex_values.size(); i++ ) {
             cout << hex_values[i] << endl;
         }
@@ -64,13 +67,14 @@ vector<pair<string, vector<string>>> parse_hex_file() {
     // extract data fields
     //unordered_map<string, vector<string>> dataFields;     // DON'T USE --> does not keep sequential order 
     vector<pair<string, vector<string>>> dataFields;        // declare map of data (hex, data fields)
+    vector<string> hex_lines;                               // extracted hex data
     for ( int i = 0; i < hex_values.size(); i++ ) {
 
         // get indices for variable-length data
         int start = 9;
         int end = (hex_values[i].size() - 2) - start;
 
-        // initialize extracted fields (ignore first semicolon)W
+        // initialize extracted fields (ignore first semicolon)
         vector<string> currFields;
         string byte_count = hex_values[i].substr(1, 2);
         currFields.push_back(byte_count);
@@ -83,29 +87,67 @@ vector<pair<string, vector<string>>> parse_hex_file() {
         string checksum = hex_values[i].substr(hex_values[i].size() - 2, 2);
         currFields.push_back(checksum);
         
-        // initialize map of data (hex, data fields)
-        dataFields.push_back(make_pair(hex_values[i], currFields));
-        dataFields[i].first = hex_values[i];
+        // initialize map of data (hex, data fields), consider only data bytes (record type == 00)
+        if ( record_type == "00" ) {
+            dataFields.push_back(make_pair(hex_values[i], currFields));
+            hex_lines.push_back(data);
+        }
     }
 
     // check extracted data
-    cout << endl << "Extracted Data" << endl << endl;
+    cout << endl << "Extracted Data" << endl;
     vector<string> fields = {"Byte Count: ", "Address: ", "Record Type: ", "Data: ", "Checksum: "};
     for ( auto hex_value : dataFields ) {
+        cout << "---------------------------------------------" << endl;
         cout << hex_value.first << endl;
         for ( int i = 0; i < hex_value.second.size(); i++ ) {
             cout << fields[i] << hex_value.second[i] << endl;
         }
-        cout << endl;
+    }
+
+    // check hex lines
+    cout << endl << "Hex Lines" << endl;
+    cout << "----------" << endl;
+    for ( int i = 0; i < hex_lines.size(); i++ ) {
+        cout << hex_lines[i] << endl;
     }
 
     // return
-    return dataFields;
+    return hex_lines;
+}
+
+// convert hex data to binary
+vector<uint8_t> hex_to_binary(vector<string> hex_lines) {
+
+    // declare variables
+    vector<string> binary_lines;
+    vector<uint8_t> binary_bytes;
+
+    // convert hexadecimal to binary
+    cout << endl << "Convert hex to binary";
+    for ( int i = 0; i < hex_lines.size(); i++ ) {
+        cout << endl << "----------------------------------------------------------------" << endl;
+        string curr_hex = hex_lines[i];
+        cout << curr_hex << endl;
+        // convert hex line to binary (split up instead of converting entire line at once)
+        for ( int j = 0; j < curr_hex.size(); j += 2 ) {
+            string byte_str = curr_hex.substr(j, 2);
+            uint8_t curr_byte = static_cast<uint8_t>(stoul(byte_str, nullptr, 16));
+            binary_bytes.push_back(curr_byte);
+            cout << (int)curr_byte << " ";
+        }
+
+        
+    }
+
+    // return
+    return binary_bytes;
 }
 
 // driver code
 int main() {
 
+    // quick check
     char test = int('a');
     cout << char(99) << endl;
     
@@ -113,8 +155,10 @@ int main() {
     //gen_bin_file();
 
     // parse hex file
-    vector<pair<string, vector<string>>> dataFields;
-    dataFields = parse_hex_file();
+    vector<string> hex_lines;
+    hex_lines = parse_hex_file();
+    vector<uint8_t> binary_bytes;
+    binary_bytes = hex_to_binary(hex_lines);
 
     // return
     return 0;
@@ -123,4 +167,14 @@ int main() {
 /*
 Notes
 - 10 KB size will show as 10,000 / 1,024 = 9.76 KB given file explorer conventions
+
+----------------------
+Intel HEX File Format
+----------------------
+Record Type                 Code	Purpose
+Data	                    00	    Actual data to be written to memory
+EOF	                        01	    End of file marker
+Extended Segment Address	02	    Used for addressing beyond 64KB
+Extended Linear Address	    04	    Used for 32-bit addressing
+Start Linear Address	    05	    Optional, for specifying execution start address
 */
